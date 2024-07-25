629aa633d90b59e1b348291aa3d171f9fe75c0f5
F-S-Uyar <142197603+F-S-Uyar@users.noreply.github.com>
2024-07-25 08:50:46 +0300

    Project stalled on handler implementation

---
+++ src/main/java/com/workintech/zoo/exceptions/ZooGlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.workintech.zoo.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ZooGlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ZooErrorResponse> handleException(ZooException exception) {
+
+    }
+}
